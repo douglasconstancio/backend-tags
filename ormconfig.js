e4a91119d802a4688b82b482a7d61c405bf0693c
Douglas <douglas.constancio@ngi.com.br>
2021-04-10 00:16:57 -0300

    fix(orm): remove console and quotation marks

--- ormconfig.js
+++ ormconfig.js
@@ -1,17 +1,15 @@
-console.log('process.env', process.env.DATABASE_URL);
-
 module.exports = {
-    "type": "postgres",
-    "url": process.env.DATABASE_URL,
-    "autoSchemaSync": true,
-    "migrations": ["dist/src/database/migrations/**.js"],
-    "entities": ["dist/src/models/**.js"],
-    "cli": {
-        "migrationsDir": "./src/database/migrations"
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    autoSchemaSync: true,
+    migrations: ['dist/src/database/migrations/**.js'],
+    entities: ['dist/src/models/**.js'],
+    cli: {
+        migrationsDir: './src/database/migrations'
     },
-    "extra": {
-        "ssl": {
-            "rejectUnauthorized": false
+    extra: {
+        ssl: {
+            rejectUnauthorized: false
         }
     }
 }
